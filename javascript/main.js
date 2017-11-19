f0c9de8d409c55c0eb3a83889a83109d8297a103
markhamilton90 <msh387@gmail.com>
2017-11-19 00:39:28 -0600

    initial

---
+++ javascript/main.js
@@ -0,0 +1,73 @@
+//░░░░░░░░░░░░░░░░░░░░░░░░░░░
+//
+//    DIRECTORY
+//
+//    _SmoothScroll
+//    _Clipboard
+//
+//░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+
+
+jQuery(document).ready(function($){
+
+    $('#hero-section h1').removeClass('faded');
+    $('#hero-section h2').removeClass('faded');
+
+    $('header .directory').removeClass('faded');
+    $('header .cta-contact').removeClass('faded');
+
+    $('#hero-section p').removeClass('faded');
+    $('#hero-section .scroll-arrow').removeClass('faded');
+
+
+
+    $(window).on('load scroll resize', function() {
+        var scrollBottom = $(window).scrollTop() + $(window).height();
+        $('.faded').each(function() {
+            if ( ($(this).offset().top + 300 ) < scrollBottom ) {
+                $(this).removeClass('faded');
+            }
+        });
+    });
+
+
+    //▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+    // _SmoothScroll
+    //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 50
+                }, 700);
+                return false;
+            }
+        }
+    });
+
+
+    //▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+    // _Clipboard
+    //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+
+    function copyToClipboard(string) {
+        var temp = $('<input>');
+        $('body').append(temp);
+        temp.val(string).select();
+        document.execCommand('copy');
+        temp.remove();
+    }
+
+    $('#clipboard').on('click', function(e) {
+        e.preventDefault();
+        copyToClipboard('msh387@gmail.com');
+    });
+
+
+// jQuery
+});
